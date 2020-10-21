76d5190bf08b26424727adf949e2f546049f3bf5
Amaan <69464744+AmaanSayyad@users.noreply.github.com>
2020-10-21 22:26:18 +0530

    Update hello_world.c

--- Hello World Programs/hello_world.c
+++ Hello World Programs/hello_world.c
@@ -8,18 +8,19 @@ int main()
     printf("Press 1 for Hello World\n");
     printf("Press 2 for Hello Hactoberfest\n");
     scanf("%d",&n);
-    switch(n){
-    case 1:
-        printf("Hello World\n");
-        printf("I'm new to open source\n");
-        break;
+    switch(n)
+    {
+      case 1:
+          printf("Hello World\n");
+          printf("I'm new to open source\n");
+          break;
 
-    case 2:
-        printf("Hello Hacktoberfest\n");
-        printf("Let's Contribute to Open Source");
-        break;
+      case 2:
+          printf("Hello Hacktoberfest\n");
+          printf("Let's Contribute to Open Source");
+          break;
 
-    default:
-        printf("Enter valid response");
+      default:
+          printf("***Your Choice is Invalid!! Enter valid response***");
     }
 }
